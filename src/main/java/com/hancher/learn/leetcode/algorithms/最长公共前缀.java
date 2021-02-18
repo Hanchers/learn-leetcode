631dd366665fc381ae116c7588a495f534aef0aa
hancher <hanchers@outlook.com>
2021-02-18 19:22:14 +0800

    最长公共前缀：prepare

---
+++ src/main/java/com/hancher/learn/leetcode/algorithms/最长公共前缀.java
@@ -0,0 +1,26 @@
+package com.hancher.learn.leetcode.algorithms;
+/**
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ *
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * https://leetcode-cn.com/problems/longest-common-prefix/
+ * <p/>
+ * @author Hancher
+ * @date Created in 2021年02月18日 18:22
+ * @version 1.0
+ * @since 1.0
+ */
+public class 最长公共前缀 {
+    public static void main(String[] args) {
+
+    }
+
+    public static String hancherSolution(String[] strs) {
+        String prefix = "";
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+
+        return "";
+    }
+}
